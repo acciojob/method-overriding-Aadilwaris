5f2cf9d2e2cc0e2c8e33117985992fe2784d4ee1
Adil Waris <101867676+Aadilwaris@users.noreply.github.com>
2022-11-27 04:55:05 +0530

    Create Method Overriding.java

---
+++ Method Overriding.java
@@ -0,0 +1,18 @@
+class A{
+  void meth(){
+    System.out.println("Invoking method from class A");
+  }
+}
+
+class B extends A{
+  void meth(){
+    System.out.println("Method is overridden in Extendend class B");
+  }
+}
+
+public class Main{
+  public static void main(String[] args){
+    B obj = new B();
+    obj.meth();
+  }
+}
